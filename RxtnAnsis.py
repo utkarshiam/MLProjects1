22cc11fb98971a4c017a51ff81603d413c5b5bd5
Utkarsh Upadhyay <utkarshupadhyay@Muditas-MacBook-Air.local>
2018-11-02 00:36:17 +0530

    fifth commit

--- RxtnAnsis.py
+++ RxtnAnsis.py
@@ -1,12 +1,13 @@
 import tweepy
 import csv
+from pandas import DataFrame
 from textblob import TextBlob
 
-con_key="" ###your key
-con_sec="" ###your secret
+con_key="KIrd3dd7BSB9UT5DNa15y2DG9" ###your key
+con_sec="YDifo1C6RmIqP9aOlR9Mi83jtCm2AbXH099vCHz2ERZKgOz50s" ###your secret
 
-acc_token="" ###your token
-acc_token_sec=""###your token secret
+acc_token="1058038441877753856-WpIEfvaiznbh0FTkntAdpAJ3i1lR53" ###your token
+acc_token_sec="VjUZI1jQYoHsRzovInyXL1tpyBg2NPIYID4WSX2MmCNZ8"###your token secret
 
 auth=tweepy.OAuthHandler(con_key,con_sec)
 auth.set_access_token(acc_token,acc_token_sec)
@@ -27,3 +28,16 @@ with open('dataset.csv', mode='w') as tweets_file:
 		print(T_text,T_user,T_c_at)
 		print(analysis)
 		tweet_writer.writerow([T_text, T_user,T_c_at, analysis ])
+
+#if want to use pandas 
+#see below
+
+# for tweet2 in bitchTweets:
+# 	DataRetrieved={
+# 		'Tweet': tweet2.text,
+# 		 'Author': tweet2.user.name,
+# 		 'Time created': tweet2.created_at,
+# 		  'Sentiment': TextBlob(tweet2.text).sentiment.polarity
+# 	}
+# df = DataFrame(DataRetrieved, columns= ['Tweet', 'Author','Time created', 'Sentiment'])
+# export_csv = df.to_csv 
