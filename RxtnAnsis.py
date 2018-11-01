8b1f2c56ea663d824de3d80a424f15c8480c9f06
Utkarsh Upadhyay <utkarshupadhyay@Muditas-MacBook-Air.local>
2018-11-02 00:21:10 +0530

    third commit

--- RxtnAnsis.py
+++ RxtnAnsis.py
@@ -2,18 +2,18 @@ import tweepy
 import csv
 from textblob import TextBlob
 
-con_key="KIrd3dd7BSB9UT5DNa15y2DG9" 
-con_sec="YDifo1C6RmIqP9aOlR9Mi83jtCm2AbXH099vCHz2ERZKgOz50s"
+con_key="" ###your key
+con_sec="" ###your secret
 
-acc_token="1058038441877753856-WpIEfvaiznbh0FTkntAdpAJ3i1lR53"
-acc_token_sec="VjUZI1jQYoHsRzovInyXL1tpyBg2NPIYID4WSX2MmCNZ8"
+acc_token="" ###your token
+acc_token_sec=""###your token secret
 
 auth=tweepy.OAuthHandler(con_key,con_sec)
 auth.set_access_token(acc_token,acc_token_sec)
 
 api=tweepy.API(auth)
 tweetTopic=input("Topic De")
-bitchTweets= api.search(tweetTopic)
+bitchTweets= api.search(tweetTopic) ###add whatever topic you like
 
 with open('dataset.csv', mode='w') as tweets_file:
 	tweet_writer= csv.writer(tweets_file, delimiter=',', quotechar='"', quoting= csv.QUOTE_MINIMAL)
